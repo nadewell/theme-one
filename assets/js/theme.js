4a6bd86b41cea4a46b8cd6db2dc0256cbf6696a3
Nishant Shaligram <nishant.shaligram@gmail.com>
2019-05-30 22:57:04 +0530

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -28,8 +28,34 @@ jQuery(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 3000,
         arrows: false,
+        dots: false,
         slidesToShow: 3,
         slidesToScroll: 1,
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+            },
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            } 
+        ]
     });
 
     // making column equal height
